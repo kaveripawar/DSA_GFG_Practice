11889c89efc8b285256d6719f1d2b1043cde394a
Kaveri Pawar <99605204+kaveripawar@users.noreply.github.com>
2023-09-26 22:53:03 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Find All Four Sum Numbers/find-all-four-sum-numbers.cpp
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& arr, int k) {
+        vector<vector<int>> result;
+        int n = arr.size();
+        
+        if (n < 4) {
+            return result; // There can't be any 4Sum with less than 4 elements.
+        }
+        
+        sort(arr.begin(), arr.end()); // Sort the input array.
+        
+        for (int i = 0; i < n - 3; i++) {
+            // Avoid duplicates for the first element.
+            if (i > 0 && arr[i] == arr[i - 1]) {
+                continue;
+            }
+            
+            for (int j = i + 1; j < n - 2; j++) {
+                // Avoid duplicates for the second element.
+                if (j > i + 1 && arr[j] == arr[j - 1]) {
+                    continue;
+                }
+                
+                int left = j + 1;
+                int right = n - 1;
+                
+                while (left < right) {
+                    int sum = arr[i] + arr[j] + arr[left] + arr[right];
+                    
+                    if (sum == k) {
+                        result.push_back({arr[i], arr[j], arr[left], arr[right]});
+                        
+                        // Avoid duplicates for the third element.
+                        while (left < right && arr[left] == arr[left + 1]) {
+                            left++;
+                        }
+                        
+                        // Avoid duplicates for the fourth element.
+                        while (left < right && arr[right] == arr[right - 1]) {
+                            right--;
+                        }
+                        
+                        left++;
+                        right--;
+                    } else if (sum < k) {
+                        left++;
+                    } else {
+                        right--;
+                    }
+                }
+            }
+        }
+        
+        return result;
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k, i;
+        cin >> n >> k;
+        vector<int> a(n);
+        for (i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        Solution ob;
+        vector<vector<int> > ans = ob.fourSum(a, k);
+        for (auto &v : ans) {
+            for (int &u : v) {
+                cout << u << " ";
+            }
+            cout << "$";
+        }
+        if (ans.empty()) {
+            cout << -1;
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
